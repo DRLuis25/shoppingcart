1c5eb886686d6adbf4f1eb434ca6b157766dc5c9
mparedesv <mparedesv@unitru.edu.pe>
2021-02-13 12:02:16 -0500

    agregue seeders

--- database/seeds/DatosSeeder.php
+++ database/seeds/DatosSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\Productos;
 use App\User;
+use App\Models\Categorias;
 use Illuminate\Database\Seeder;
 
 class DatosSeeder extends Seeder
@@ -19,7 +20,30 @@ class DatosSeeder extends Seeder
             'email'=>'admin@gmail.com',
             'password'=>'$2y$10$cuf37o9lN0IkRFv73Q7IB.c5bDqCvog845XuTKHxSbMep/D04mknG' //password
         ]);
-        //Docentes
-        factory(Productos::class,20)->create();
+        //categorias
+        $categoria1=Categorias::create([
+            'descripcion'=>'bebidas',
+        ]);
+        $categoria2=Categorias::create([
+            'descripcion'=>'abarrotes',
+        ]);
+        $categoria3=Categorias::create([
+            'descripcion'=>'ga',
+        ]);
+        $categoria4=Categorias::create([
+            'descripcion'=>'gaes',
+        ]);
+        $categoria5=Categorias::create([
+            'descripcion'=>'asdasdasd',
+        ]);
+
+        //productos
+        $producto=Productos::create([
+            'descripcion'=>'zapato',
+            'foto'=>null,
+            'stock'=>'50',
+            'precio'=>'10',
+            'categoria_id'=>$categoria1->id,
+        ]);
     }
 }
